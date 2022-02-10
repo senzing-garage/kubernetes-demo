a72ae083a31fa5e2210a30bf2814bb860a55887c
michael@dockter.com <michael@dockter.com>
2022-02-10 15:40:55 -0500

    #100 Add script

---
+++ bin/airgapped/populate-minikube-registry.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Instantiate "DOCKER_IMAGES", a list of docker images to be installed into the minikube registry.
+
+source ${SENZING_AIRGAPPED_DIR}/airgapped/docker-images.sh
+
+# Process each docker image.
+
+for DOCKER_IMAGE in ${DOCKER_IMAGES[@]};
+do
+
+    # Push images into minikube registry.
+    # https://minikube.sigs.k8s.io/docs/commands/image/#minikube-image-load
+
+    minikube image load ${DOCKER_IMAGE}
+done
+
+# List images in the minikube docker registry.
+# https://minikube.sigs.k8s.io/docs/commands/image/#minikube-image-ls
+
+minikube image ls
