5cdaaed36edbd86291f88b825f0d5350c6c35f4e
docktermj <michael@dockter.com>
2022-04-25 17:16:08 -0400

    #104 Update kafka-postresql instructions

---
+++ bin/docker-images-for-helm-kafka-postgresql.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# List all of the docker images to be installed into the minikube registry.
+
+DOCKER_IMAGES=(
+    "bitnami/kafka:${SENZING_DOCKER_IMAGE_VERSION_BITNAMI_KAFKA:-latest}"
+    "bitnami/postgresql:${SENZING_DOCKER_IMAGE_VERSION_BITNAMI_POSTGRESQL:-latest}"
+    "confluentinc/cp-kafka:${SENZING_DOCKER_IMAGE_VERSION_CONFLUENTINC_CP_KAFKA:-latest}"
+    "dpage/pgadmin4:${SENZING_DOCKER_IMAGE_VERSION_DPAGE_PGADMIN4:-latest}"
+    "senzing/apt:${SENZING_DOCKER_IMAGE_VERSION_APT:-latest}"
+    "senzing/configurator:${SENZING_DOCKER_IMAGE_VERSION_CONFIGURATOR:-latest}"
+    "senzing/entity-search-web-app:${SENZING_DOCKER_IMAGE_VERSION_ENTITY_SEARCH_WEB_APP:-latest}"
+    "senzing/init-container:${SENZING_DOCKER_IMAGE_VERSION_INIT_CONTAINER:-latest}"
+    "senzing/installer:${SENZING_VERSION_SENZINGAPI:-latest}"
+    "senzing/postgresql-client:${SENZING_DOCKER_IMAGE_VERSION_POSTGRESQL_CLIENT:-latest}"
+    "senzing/redoer:${SENZING_DOCKER_IMAGE_VERSION_REDOER:-latest}"
+    "senzing/senzing-api-server:${SENZING_DOCKER_IMAGE_VERSION_SENZING_API_SERVER:-latest}"
+    "senzing/senzing-base:${SENZING_DOCKER_IMAGE_VERSION_SENZING_BASE:-latest}"
+    "senzing/senzing-console:${SENZING_DOCKER_IMAGE_VERSION_SENZING_CONSOLE:-latest}"
+    "senzing/stream-loader:${SENZING_DOCKER_IMAGE_VERSION_STREAM_LOADER:-latest}"
+    "senzing/stream-producer:${SENZING_DOCKER_IMAGE_VERSION_STREAM_PRODUCER:-latest}"
+    "senzing/yum:${SENZING_DOCKER_IMAGE_VERSION_YUM:-latest}"
+    "swaggerapi/swagger-ui:${SENZING_DOCKER_IMAGE_VERSION_SWAGGERAPI_SWAGGER_UI:-latest}"
+)
