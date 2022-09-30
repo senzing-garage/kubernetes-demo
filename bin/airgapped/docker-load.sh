942eab25529c9675c9e76de7d4206e787de18ddb
docktermj <michael@dockter.com>
2022-09-30 14:50:04 -0400

    #118 Savepoint

--- bin/airgapped/docker-load.sh
+++ bin/airgapped/docker-load.sh
@@ -25,23 +25,6 @@ if [[ ${ERRORS} > 0 ]]; then
     exit 1
 fi
 
-# Identify subdirectories for each repository.
+# Load docker images.
 
-DOCKER_REGISTRIES=(
-    "bitnami"
-    "dpage"
-    "senzing"
-    "swaggerapi"
-)
-
-# For each Docker registry:
-
-for DOCKER_REGISTRY in ${DOCKER_REGISTRIES[@]}; do
-
-    # For each image in the Docker registry, load image into local Docker registry.
-
-    for DOCKER_IMAGE in ${SENZING_AIRGAPPED_DIR}/docker-images/${DOCKER_REGISTRY}/* ; do
-        echo "${DOCKER_IMAGE}"
-        ${SENZING_SUDO} docker load --input ${DOCKER_IMAGE}
-    done
-done
+${SENZING_SUDO} docker load --input ${SENZING_AIRGAPPED_DIR}/docker-images.tar
